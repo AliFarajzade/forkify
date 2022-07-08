6a2b9bdf46b8d46c918f329da2a4383cd20bf5ed
Ali <alifarajzade1@yahoo.com>
2022-07-08 15:46:56 +0430

    Initial commit.

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '278c915f-7d41-4196-9fae-f5b3057fe8a4';
+export const MODAL_CLOSE_SEC = 2.5;
